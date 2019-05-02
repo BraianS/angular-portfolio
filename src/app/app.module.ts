211aab4b848f78253349f6fca06687ebac348f49
BraianS <braiannogueirasilva@gmail.com>
2019-05-02 09:56:36 -0300

    Drop app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
@@ -21,7 +20,6 @@ import { SkillComponent } from './skill/skill.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
