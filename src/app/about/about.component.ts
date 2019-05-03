79f586bad939f87938414afdc81462d0203aabea
BraianS <braiannogueirasilva@gmail.com>
2019-05-03 16:08:25 -0300

    Add breakpoints with screen size
    Create mixin to use the breakpoints with different screen size.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,7 +10,7 @@ export class AboutComponent implements OnInit {
   constructor() { }
   title: string = "Ola, meu nome é Braian ";
   paragraph: string = "Sou formado em Tecnologia em Análise e Desenvolvimento de Sistemas pela "+
-   "faculdade Anhanguera de Sorocaba, meu objetivo é me tornar desenvolvedor full-stack";
+   "faculdade Anhanguera de Sorocaba, meu objetivo é me tornar desenvolvedor full-stack.";
 
   ngOnInit() {}
 
